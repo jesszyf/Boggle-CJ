fefd70f63ca9ac121b00b790cb68de2d68140b78
jesszyf <feng22z@mtholyoke.edu>
2017-01-13 14:16:04 -0500

    start-main-final

--- app/src/main/java/com/example/demouser/boggle_cj/MainActivity.java
+++ app/src/main/java/com/example/demouser/boggle_cj/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.demouser.boggle_cj;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,7 +14,22 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        ((Button)findViewById(R.id.checkButton)).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v)
+            {
+                check();
+            }
+        });
+
+    }
+
+    private void check()
+    {
+        startActivity(new Intent(this, FinalActivity.class));
+    }
+
 
         // Hi we are making changes
-    }
+
 }
