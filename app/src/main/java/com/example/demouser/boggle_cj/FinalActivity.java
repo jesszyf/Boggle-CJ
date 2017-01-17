7423d9d475eec3659a0d05a1cafafc10dea52108
jesszyf <feng22z@mtholyoke.edu>
2017-01-16 22:44:27 -0500

    Button actionListener added
    - Click the buttons in grid and add its content to the typed word String
    - Added play again button in Final Activity, reset typedWord String

--- app/src/main/java/com/example/demouser/boggle_cj/FinalActivity.java
+++ app/src/main/java/com/example/demouser/boggle_cj/FinalActivity.java
@@ -1,7 +1,10 @@
 package com.example.demouser.boggle_cj;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class FinalActivity extends AppCompatActivity {
 
@@ -9,5 +12,20 @@ public class FinalActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_final);
+
+        ((Button) findViewById(R.id.resetButton)).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                reset();
+            }
+        });
     }
+
+        // reset method, from final activity back to start activity
+    private void reset()
+    {
+        startActivity(new Intent(this, StartActivity.class));
+
+    }
+
 }
--- app/src/main/java/com/example/demouser/boggle_cj/MainActivity.java
+++ app/src/main/java/com/example/demouser/boggle_cj/MainActivity.java
@@ -2,8 +2,11 @@ package com.example.demouser.boggle_cj;
 
 
 import android.content.res.AssetManager;
+import android.graphics.ColorFilter;
+import android.graphics.PorterDuff;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import java.io.IOException;
@@ -19,12 +22,15 @@ import android.widget.Button;
 public class MainActivity extends AppCompatActivity {
 
     private BoogleDictionary dictionary;
+    public static String typedWord = "";
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        typedWord = "";
+
 
         ((Button)findViewById(R.id.checkButton)).setOnClickListener(new View.OnClickListener() {
             @Override
@@ -33,6 +39,238 @@ public class MainActivity extends AppCompatActivity {
                 check();
             }
         });
+
+        // on click listener for all the buttons in the grid
+        ((Button)findViewById(R.id.button00)).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v)
+            {
+                // add the on button to typedWord String
+                typedWord += ((Button)findViewById(R.id.button00)).getText().toString();
+                // update text view
+                ((TextView)findViewById(R.id.typedWord)).setText(typedWord);
+                // set the button to a clicked color
+                ((Button)findViewById(R.id.button00)).setBackgroundColor(getResources().getColor(R.color.clickedButton));
+
+            }
+        });
+
+        ((Button)findViewById(R.id.button01)).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v)
+            {
+                // add the on button to typedWord String
+                typedWord += ((Button)findViewById(R.id.button01)).getText().toString();
+                // update text view
+                ((TextView)findViewById(R.id.typedWord)).setText(typedWord);
+                // set the button to a clicked color
+                ((Button)findViewById(R.id.button01)).setBackgroundColor(getResources().getColor(R.color.clickedButton));
+
+            }
+        });
+
+        ((Button)findViewById(R.id.button02)).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v)
+            {
+                // add the on button to typedWord String
+                typedWord += ((Button)findViewById(R.id.button02)).getText().toString();
+                // update text view
+                ((TextView)findViewById(R.id.typedWord)).setText(typedWord);
+                // set the button to a clicked color
+                ((Button)findViewById(R.id.button02)).setBackgroundColor(getResources().getColor(R.color.clickedButton));
+
+            }
+        });
+
+        ((Button)findViewById(R.id.button03)).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v)
+            {
+                // add the on button to typedWord String
+                typedWord += ((Button)findViewById(R.id.button03)).getText().toString();
+                // update text view
+                ((TextView)findViewById(R.id.typedWord)).setText(typedWord);
+                // set the button to a clicked color
+                ((Button)findViewById(R.id.button03)).setBackgroundColor(getResources().getColor(R.color.clickedButton));
+
+            }
+        });
+
+        ((Button)findViewById(R.id.button10)).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v)
+            {
+                // add the on button to typedWord String
+                typedWord += ((Button)findViewById(R.id.button10)).getText().toString();
+                // update text view
+                ((TextView)findViewById(R.id.typedWord)).setText(typedWord);
+                // set the button to a clicked color
+                ((Button)findViewById(R.id.button10)).setBackgroundColor(getResources().getColor(R.color.clickedButton));
+
+            }
+        });
+
+        ((Button)findViewById(R.id.button11)).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v)
+            {
+                // add the on button to typedWord String
+                typedWord += ((Button)findViewById(R.id.button11)).getText().toString();
+                // update text view
+                ((TextView)findViewById(R.id.typedWord)).setText(typedWord);
+                // set the button to a clicked color
+                ((Button)findViewById(R.id.button11)).setBackgroundColor(getResources().getColor(R.color.clickedButton));
+
+            }
+        });
+
+        ((Button)findViewById(R.id.button12)).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v)
+            {
+                // add the on button to typedWord String
+                typedWord += ((Button)findViewById(R.id.button12)).getText().toString();
+                // update text view
+                ((TextView)findViewById(R.id.typedWord)).setText(typedWord);
+                // set the button to a clicked color
+                ((Button)findViewById(R.id.button12)).setBackgroundColor(getResources().getColor(R.color.clickedButton));
+
+            }
+        });
+
+        ((Button)findViewById(R.id.button13)).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v)
+            {
+                // add the on button to typedWord String
+                typedWord += ((Button)findViewById(R.id.button13)).getText().toString();
+                // update text view
+                ((TextView)findViewById(R.id.typedWord)).setText(typedWord);
+                // set the button to a clicked color
+                ((Button)findViewById(R.id.button13)).setBackgroundColor(getResources().getColor(R.color.clickedButton));
+
+            }
+        });
+
+        ((Button)findViewById(R.id.button20)).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v)
+            {
+                // add the on button to typedWord String
+                typedWord += ((Button)findViewById(R.id.button20)).getText().toString();
+                // update text view
+                ((TextView)findViewById(R.id.typedWord)).setText(typedWord);
+                // set the button to a clicked color
+                ((Button)findViewById(R.id.button20)).setBackgroundColor(getResources().getColor(R.color.clickedButton));
+
+            }
+        });
+
+        ((Button)findViewById(R.id.button21)).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v)
+            {
+                // add the on button to typedWord String
+                typedWord += ((Button)findViewById(R.id.button21)).getText().toString();
+                // update text view
+                ((TextView)findViewById(R.id.typedWord)).setText(typedWord);
+                // set the button to a clicked color
+                ((Button)findViewById(R.id.button21)).setBackgroundColor(getResources().getColor(R.color.clickedButton));
+
+            }
+        });
+
+
+        ((Button)findViewById(R.id.button22)).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v)
+            {
+                // add the on button to typedWord String
+                typedWord += ((Button)findViewById(R.id.button22)).getText().toString();
+                // update text view
+                ((TextView)findViewById(R.id.typedWord)).setText(typedWord);
+                // set the button to a clicked color
+                ((Button)findViewById(R.id.button22)).setBackgroundColor(getResources().getColor(R.color.clickedButton));
+
+            }
+        });
+
+        ((Button)findViewById(R.id.button23)).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v)
+            {
+                // add the on button to typedWord String
+                typedWord += ((Button)findViewById(R.id.button23)).getText().toString();
+                // update text view
+                ((TextView)findViewById(R.id.typedWord)).setText(typedWord);
+                // set the button to a clicked color
+                ((Button)findViewById(R.id.button23)).setBackgroundColor(getResources().getColor(R.color.clickedButton));
+
+            }
+        });
+
+        ((Button)findViewById(R.id.button30)).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v)
+            {
+                // add the on button to typedWord String
+                typedWord += ((Button)findViewById(R.id.button30)).getText().toString();
+                // update text view
+                ((TextView)findViewById(R.id.typedWord)).setText(typedWord);
+                // set the button to a clicked color
+                ((Button)findViewById(R.id.button30)).setBackgroundColor(getResources().getColor(R.color.clickedButton));
+
+            }
+        });
+
+        ((Button)findViewById(R.id.button31)).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v)
+            {
+                // add the on button to typedWord String
+                typedWord += ((Button)findViewById(R.id.button31)).getText().toString();
+                // update text view
+                ((TextView)findViewById(R.id.typedWord)).setText(typedWord);
+                // set the button to a clicked color
+                ((Button)findViewById(R.id.button31)).setBackgroundColor(getResources().getColor(R.color.clickedButton));
+
+            }
+        });
+
+        ((Button)findViewById(R.id.button32)).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v)
+            {
+                // add the on button to typedWord String
+                typedWord += ((Button)findViewById(R.id.button32)).getText().toString();
+                // update text view
+                ((TextView)findViewById(R.id.typedWord)).setText(typedWord);
+                // set the button to a clicked color
+                ((Button)findViewById(R.id.button32)).setBackgroundColor(getResources().getColor(R.color.clickedButton));
+
+            }
+        });
+
+        ((Button)findViewById(R.id.button33)).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v)
+            {
+                // add the on button to typedWord String
+                typedWord += ((Button)findViewById(R.id.button33)).getText().toString();
+                // update text view
+                ((TextView)findViewById(R.id.typedWord)).setText(typedWord);
+                // set the button to a clicked color
+                ((Button)findViewById(R.id.button33)).setBackgroundColor(getResources().getColor(R.color.clickedButton));
+
+            }
+        });
+
+
+
+
+
+
 
 
         //load the words.txt file
@@ -48,10 +286,17 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
+    // method to go to final Activity
     private void check()
     {
+
         startActivity(new Intent(this, FinalActivity.class));
+
+
     }
+
+
+
 
 
         // Hi we are making changes
